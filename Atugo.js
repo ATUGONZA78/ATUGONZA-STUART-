1542a48c9703a517d5a64a8eb0de08852e2675d5
ATUGONZA78 <116483728+ATUGONZA78@users.noreply.github.com>
2022-11-18 14:51:57 +0300

    Add files via upload

---
+++ Atugo.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>my page</title>
+</head>
+<body>
+<script>
+var Myname=prompt("enter your name");
+document write ("hello" +Myname);
+
+</script>
+
+</body>
+</html>
